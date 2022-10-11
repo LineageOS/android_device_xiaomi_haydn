c11c95a7f7358105ff16655a4e09d61ebcdc8fb8
AdarshGrewal <adarshgrewal@gmail.com>
2022-10-12 04:01:20 +0530

    haydn: Initial device tree
    Change-Id: I674ddba9e29525327e2a4b9d0c4fe34e42894891

---
+++ lineage_haydn.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from haydn device
+$(call inherit-product, device/xiaomi/haydn/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := haydn
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := M2012K11G
+PRODUCT_NAME := lineage_haydn
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
