da8daae3392c77ee4c654acbdc2a716a5fd5ca87
AdarshGrewal <adarshgrewal@gmail.com>
2022-10-12 03:58:01 +0530

    haydn: Define dtbo partition size
    Change-Id: I171b940d676a6746e0c6295171f75144572ddb7e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := haydn
 
+# Partitions
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
+
 # Include proprietary files
 include vendor/xiaomi/haydn/BoardConfigVendor.mk
