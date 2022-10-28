2ec9b1fa682053355a0df4d85dbadf1ce26a5167
AdarshGrewal <adarshgrewal@gmail.com>
2022-10-28 15:29:21 +0530

    haydn: overlays: Import Brightness overlays
    Change-Id: I85c5bec957e4af2527b465f6f7e7f879678464b0

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
