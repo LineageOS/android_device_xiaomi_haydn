999cf37559f14a16d7b0c716b79b577233478d1e
AdarshGrewal <adarshgrewal@gmail.com>
2022-10-30 19:27:31 +0530

    haydn: Disable content detection for refresh rate
    Change-Id: I24641e8e3b6e9b80328cdf1c0fa65d9a16085acd

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_PACKAGES += \
     HaydnproCNSettingsProviderOverlay \
     HaydnproCNWifiOverlay
 
+# Refresh Rate
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=false
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
