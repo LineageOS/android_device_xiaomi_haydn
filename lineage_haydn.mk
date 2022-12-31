bd3e53e442ff26065a7e84bb807b7a954b316876
Arian <arian.kulmer@web.de>
2022-12-31 16:41:46 +0100

    haydn: Update blobs to V13.0.4.0.SKKMIXM
    Change-Id: I2c2d80b44d67ddd3103ee05ab4299cefe8ec6026

--- lineage_haydn.mk
+++ lineage_haydn.mk
@@ -21,8 +21,8 @@ PRODUCT_MODEL := M2012K11G
 PRODUCT_NAME := lineage_haydn
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="haydn_global-user 12 SKQ1.211006.001 V13.0.3.0.SKKMIXM release-keys"
+    PRIVATE_BUILD_DESC="haydn_global-user 12 SKQ1.211006.001 V13.0.4.0.SKKMIXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/haydn_global/haydn:12/SKQ1.211006.001/V13.0.3.0.SKKMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/haydn_global/haydn:12/SKQ1.211006.001/V13.0.4.0.SKKMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
