b81885dd9cfe6dcc2d920c7f763775ce8ff6de6b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    haydn: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Vibrator
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.xiaomi)
-$(call soong_config_set,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/haydn/haydn-vendor.mk)
